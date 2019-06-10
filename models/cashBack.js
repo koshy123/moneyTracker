e3f7e9501427b89fa6d367bbeea23ed25fce0971
salmonshoe <rafaelreynoso273@gmail.com>
2019-06-09 22:39:49 -0400

    Create Model-View-Controller Setup for project. Integrate Sequelize as the connecction to our database and begin the serer-side code

---
+++ models/cashBack.js
@@ -0,0 +1,5 @@
+module.exports = function(sequelize, DataTypes) {
+    const Reimbursement = sequelize.define('Reimbursement', {
+        // order number, amount, description, captainID gets associated
+    })
+}
